843281009846c93721772127ebdf5cfa2602ad9a
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-05-01 20:32:47 +0400

    Add select Barber

--- Sinatra/app.rb
+++ Sinatra/app.rb
@@ -19,12 +19,14 @@ post '/visit' do
     @user_name = params[:user_name].capitalize
     @phone     = params[:phone]
     @date_time = params[:date_time]
+    @barber    = params[:barber]
 
     @title = 'Спасибо!'
     @message = "Спасибо вам, #{@user_name}, будем ждать Вас."
 
     out_f = File.open './public/users.txt', 'a'
     out_f.write "User: #{@user_name}, Phone: #{@phone},"
+    out_f.write " Barber: #{@barber}"
     out_f.write " Date_Time: #{@date_time}\n"
     out_f.close
 
