1838120b37b4c393eb099a64048891bae230d835
OPR-A-RUBin <opr.a.ruby@gmail.com>
2022-05-01 20:35:10 +0400

    refactoring

--- Sinatra/app.rb
+++ Sinatra/app.rb
@@ -4,39 +4,40 @@ require 'sinatra'
 #require 'sinatra/reloader'
 
 get '/' do
-	erb :index
+  erb :index
 end
 
 get '/about' do
-	erb :about
+  erb :about
 end 
 
 get '/visit' do
-	erb :visit
+  erb :visit
 end 
 
 post '/visit' do
-    @user_name = params[:user_name].capitalize
-    @phone     = params[:phone]
-    @date_time = params[:date_time]
-    @barber    = params[:barber]
+  @user_name = params[:user_name].capitalize
+  @phone     = params[:phone]
+  @date_time = params[:date_time]
+  @barber    = params[:barber]
 
-    @title = 'Спасибо!'
-    @message = "Спасибо вам, #{@user_name}, будем ждать Вас."
+  @title = 'Спасибо!'
+  @message = "Спасибо вам, #{@user_name}, будем ждать Вас."
 
-    out_f = File.open './public/users.txt', 'a'
-    out_f.write "User: #{@user_name}, Phone: #{@phone},"
-    out_f.write " Barber: #{@barber}"
-    out_f.write " Date_Time: #{@date_time}\n"
-    out_f.close
+  out_f = File.open './public/users.txt', 'a'
+  out_f.write "User: #{@user_name}, Phone: #{@phone},"
+  out_f.write " Barber: #{@barber}"
+  out_f.write " Date_Time: #{@date_time}\n"
+  out_f.close
 
-    erb :message 
+  erb :message 
 end	
 
 get '/contacts' do
-	erb :contacts
+  erb :contacts
 end 
 
 get '/memory' do
-	erb :memory
+  out_f = File.open './public/users.txt', 'a'
+  erb :memory
 end 	
